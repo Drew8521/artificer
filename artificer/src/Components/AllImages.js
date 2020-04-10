46ddfe231b3dfe6e03ae6bfd9b1bfbffe5fc6fcd
Andrew Seaman <andrew.seaman@mullenmail.com>
2020-04-10 15:05:06 -0700

    Allowed for the app to deploy

--- artificer/src/Components/AllImages.js
+++ artificer/src/Components/AllImages.js
@@ -9,22 +9,23 @@ let listRef = storageRef.child('images/');
 
 let images = [...Array(3)];
 let i = 0;
-
+let lastUrl = "";
 // Find all the prefixes and items.
 listRef.listAll().then(function(res) {
   res.items.forEach(function(itemRef) {
     itemRef.getDownloadURL().then(function(url) {
-
+      lastUrl = url;
     });
   });
 }).catch(function(error) {
   console.log(error);
 });
 
- console.log(images);
+ console.log(lastUrl);
 
  return (
    <div>
+        <img src={lastUrl} />;
    </div>
  )
 
