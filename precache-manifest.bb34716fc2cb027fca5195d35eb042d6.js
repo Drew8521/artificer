7c4f4595055552a243ec2050dd1d9dd1a7cbb0d8
Andrew Seaman <andrew.seaman@mullenmail.com>
2020-04-09 18:40:42 -0700

    Updates

---
+++ precache-manifest.bb34716fc2cb027fca5195d35eb042d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8c56caf671a7630d73947e84d67667",
+    "url": "/Artificer/index.html"
+  },
+  {
+    "revision": "add212e6127451db40c8",
+    "url": "/Artificer/static/css/main.5ece4445.chunk.css"
+  },
+  {
+    "revision": "41c8124935a367631915",
+    "url": "/Artificer/static/js/2.4b1de018.chunk.js"
+  },
+  {
+    "revision": "4c6b4302c8e4ea71915df2de809f5a34",
+    "url": "/Artificer/static/js/2.4b1de018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add212e6127451db40c8",
+    "url": "/Artificer/static/js/main.840369c7.chunk.js"
+  },
+  {
+    "revision": "7bb505fb94d9996e8037",
+    "url": "/Artificer/static/js/runtime-main.1e7815a4.js"
+  }
+]);
